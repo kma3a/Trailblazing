9e4ca3463c581a3cdf09e643199b9a4858ee3563
Kelsey Ando <kelsey.ando@detroitlabs.com>
2017-05-17 13:00:02 -0400

    Adding the user location and making it centered on the map

--- app/src/main/java/com/trailblazing/trailblazing/MainActivity.java
+++ app/src/main/java/com/trailblazing/trailblazing/MainActivity.java
@@ -1,7 +1,13 @@
 package com.trailblazing.trailblazing;
 
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,13 +16,25 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient mGoogleApiClient;
+    private GoogleMap map;
+    private Location userLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
@@ -24,16 +42,54 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(GoogleMap map) {
+        this.map = map;
+        moveCameraIfReady();
+    }
 
-        CameraPosition googlePlex = CameraPosition.builder()
-                .target(new LatLng(37.4219999,-122.0862462))
-                .zoom(16)
-                .bearing(0)
-                .tilt(45)
-                .build();
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
 
 
-        map.moveCamera(CameraUpdateFactory.newCameraPosition(googlePlex));
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            userLocation = mLastLocation;
+            moveCameraIfReady();
+        } else {
+            Toast.makeText(this, "I have a sad panda face!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void moveCameraIfReady() {
+        if (map != null && userLocation != null) {
+            CameraPosition googlePlex = CameraPosition.builder()
+                    .target(new LatLng(userLocation.getLatitude(),userLocation.getLongitude()))
+                    .zoom(16)
+                    .bearing(0)
+                    .tilt(45)
+                    .build();
+            map.moveCamera(CameraUpdateFactory.newCameraPosition(googlePlex));
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
