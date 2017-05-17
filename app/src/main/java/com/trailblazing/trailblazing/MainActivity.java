93eb025eda7d08c466c6570708a2fda1536d60c5
Kelsey Ando <kelsey.ando@detroitlabs.com>
2017-05-17 00:11:05 -0400

    adding code to get closer to making the center be the persons current location

--- app/src/main/java/com/trailblazing/trailblazing/MainActivity.java
+++ app/src/main/java/com/trailblazing/trailblazing/MainActivity.java
@@ -1,13 +1,39 @@
 package com.trailblazing.trailblazing;
 
 import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+
+        CameraPosition googlePlex = CameraPosition.builder()
+                .target(new LatLng(37.4219999,-122.0862462))
+                .zoom(16)
+                .bearing(0)
+                .tilt(45)
+                .build();
+
+
+        map.moveCamera(CameraUpdateFactory.newCameraPosition(googlePlex));
+
     }
 }
